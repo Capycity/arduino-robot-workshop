ccbed91c41f8bafa3e1800dc98d014f81b6ad69b
Markus Hülß <markus.huelss@devdungeon.de>
2023-02-14 02:12:15 +0100

    add --data-dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 find / -name eisvogel.latex -maxdepth 10
 pandoc --version
-pandoc /README.md --template=eisvogel -o /github/workspace/README.pdf --verbose
+pandoc /README.md --data-dir=/.pandoc/templates/eisvogel.latex --template=eisvogel -o /github/workspace/README.pdf --verbose
